e036aa27ad8b280acba10c34ed1f8f33768ea3df
Guy <Ofek.r.guy@gmail.com>
2021-05-13 22:13:00 +0300

    working

--- nodes.py
+++ nodes.py
@@ -5,45 +5,56 @@ from svgwrite.container import *
 from svgwrite.path import *
 import random
 import copy
+from math import *
 
-GRID = 20
-DIST = 25
+
+GRID = 40
+DIST = 20
 OFFSET = DIST
 
-SNAKES = 30
+SNAKES = 20
 
-COLORS = ['9b59b6','3498db','2ecc71','1abc9c','f1c40f','e74c3c','e67e22']
+
+BG= '#111111'
+COLORS = [	'9b59b6','3498db','2ecc71','1abc9c','f1c40f','e74c3c','e67e22']
+# COLORS = ['000000']
 COLORS = list(map(lambda x: '#'+x,COLORS))
 nodes = None
 l = None
 
 MAX_LENGTH = 15
 
-def RandomEmptyNode(overrides=[]):
+def findConnected(start,o = [],connected = set()):
+	connected.add(start)
+	for c in start.connections:
+		if c not in o:
+			findConnected(c,[start]+o,connected)
+	return connected
+
+def RandomEmptyNode(start=None):
+	if False: # start
+		s = findConnected(start)
+	else:
+		s=l
 	node= None
 	while True:
-		node = random.choice(l)
-		if len(node.connections)==4 and not node in overrides:
+		node = random.choice(s)
+		if len(node.connections)==4 and node != start:
 			break
 	return node
 
 def findPaths(start,end,overrides=[]):
 	if end in start.connections:
-		return [[end]]
+		return [end]
 	else:
 		options = []
-		for connection in start.connections:
+		random.shuffle(start.connections)
+		for connection in sorted(start.connections,key = lambda a: (end.coords-a.coords).length()):
 			if connection not in overrides:
-				paths = findPaths(connection,end,[start]+overrides)
-				if paths:
-					options+=[[connection]+path for path in paths]
-	return options
-
-def getPath(start,end):
-	paths = findPaths(start,end)
-	paths.sort(key=lambda l:len(l))
-	return paths[0]
-
+				path = findPaths(connection,end,[start]+overrides)
+				if path:
+					return [connection]+path
+	return False
 
 
 class Point():
@@ -63,13 +74,31 @@ class Point():
 	def __add__(self,other):
 		return Point(self.x+other.x,self.y+other.y)
 
+	def __mul__(self,other):
+		return Point(self.x*other,self.y*other)
+
 	def __repr__(self):
 		return f"P({self.x},{self.y})"
+
+	def __eq__(self,obj):
+		return isinstance(obj, Point) and obj.x == self.x and obj.y==self.y
+
+	def length(self):
+		return abs(self.x)+abs(self.y)
+
+	def angle(self):
+		return atan2(self.y,self.x)
+
+	def __hash__(self):
+		return hash((self.x,self.y))
+
 
 class Snake():
 	RADIUS = 5
 	WIDTH = 3
-	CORNER = 0.3
+	CORNER = DIST/4
+	TRI_H = 0.5
+	TRI_W = 0.3
 
 	start = (0,0) # start coordinate
 	color = None
@@ -80,9 +109,21 @@ class Snake():
 
 		start = RandomEmptyNode()
 		self.start = start.coords
-		end = RandomEmptyNode([start])
-		path = getPath(start,end)
-		print(path)
+		end = None
+		path = None
+		while not path:
+			end = RandomEmptyNode(start)
+			path = findPaths(start,end)
+
+		remove(end)
+		remove(start)
+		path.insert(0,start)
+		for i in range(1,len(path)-1):
+			n,c,p = path[i+1],path[i],path[i-1]
+			unlink(c,n)
+			if c.coords-p.coords!= n.coords-c.coords:
+				remove(c)
+
 
 		self.path = [node.coords for node in path]
 
@@ -94,16 +135,37 @@ class Snake():
 
 			deltas = [self.path[i]-self.path[i-1] for i in range(1,len(self.path))]
 
-			#print(deltas)
+			commands=[]
 
-			for point in self.path:
-				path.push(('L',point.pixel()[0],point.pixel()[1]))
+			prev_delta = None
+			i=0
+			for i in range(len(self.path)):
+				if 0<i<len(self.path)-1 and deltas[i-1] != deltas[i]:
+					to = deltas[i-1]
+					fr = deltas[i]
+					pos = self.path[i]
+					MAGIC = 0.552284749831*Snake.CORNER
+					path.push(('L',pos.pixel()[0]-Snake.CORNER*to.x,pos.pixel()[1]-Snake.CORNER*to.y))
+					target = (pos.pixel()[0]+Snake.CORNER*fr.x,pos.pixel()[1]+Snake.CORNER*fr.y)
+					path.push(('C',pos.pixel()[0]-MAGIC*to.x,pos.pixel()[1]-MAGIC*to.y,pos.pixel()[0]+MAGIC*fr.x,pos.pixel()[1]+MAGIC*fr.y,target[0],target[1]))
+					
+					# direction = '+' if (to+fr).angle()>0 else '-'
+					# path.push_arc(target,90,Snake.CORNER,False,direction,True)
+				else:
+					path.push(('L',self.path[i].pixel()[0],self.path[i].pixel()[1]))
 
 			border = copy.deepcopy(path)
-			border.update({'stroke-width':Snake.WIDTH*2,'stroke':'#ffffff'})
+			border.update({'stroke-width':Snake.WIDTH*3,'stroke':BG})
 
 			g.add(border)
 			g.add(path)
+
+			end = self.path[-1]
+			forward = deltas[-1]*Snake.TRI_H
+			side = Point(forward.y,forward.x)*(Snake.TRI_W/Snake.TRI_H)
+			triangle = Path([('M',(end+side).pixel()),('L',(end+forward).pixel()),('L',(end-side).pixel())],fill=self.color)
+
+			g.add(triangle)
 
 			circle = Circle(self.start.pixel(),Snake.RADIUS,fill=self.color)
 			g.add(circle)
@@ -127,6 +189,7 @@ def ctp(coord):
 def main():
 	l = (GRID*DIST) + (OFFSET*2)
 	dwg = Drawing('res.svg',height=l, width=l)
+	dwg.add(dwg.rect(insert=(0, 0), size=('100%', '100%'), rx=None, ry=None, fill=BG))
 	gen_grid(GRID)
 
 	snakes = [Snake() for i in range(SNAKES)]
@@ -140,7 +203,8 @@ def remove(node):
 	for c in node.connections:
 		if node in c.connections:
 			c.connections.remove(node)
-	l.remove(node)
+	if node in l:
+		l.remove(node)
 
 def unlink(a,b):
 	if a in b.connections:
